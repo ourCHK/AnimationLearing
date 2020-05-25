e9eef0f671a95993fb99cd13d97f4fbf66796af0
CHK <chenghuangkai@163.com>
2020-05-25 11:36:45 +0800

    init commit

---
+++ app/src/main/java/com/chk/animationlearning/tween/TweenAnimationActivity.java
@@ -0,0 +1,16 @@
+package com.chk.animationlearning.tween;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chk.animationlearning.R;
+
+public class TweenAnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tween_animation);
+    }
+}
