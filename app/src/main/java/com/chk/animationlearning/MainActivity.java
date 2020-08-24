0bd9b941e9ff335a11555e7bde669c9b7af355fd
CHK <chenghuangkai@163.com>
2020-08-24 11:52:36 +0800

    新增lottie动画

--- app/src/main/java/com/chk/animationlearning/MainActivity.java
+++ app/src/main/java/com/chk/animationlearning/MainActivity.java
@@ -1,14 +1,47 @@
 package com.chk.animationlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.chk.animationlearning.ui.linearGradient.LinearGradientActivity;
+import com.chk.animationlearning.ui.lottie.LottieActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.LinearGradientLearning)
+    Button gradientLearning;
+
+    @BindView(R.id.LottieLearning)
+    Button lottieLearning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
+
+    @OnClick({R.id.LinearGradientLearning,R.id.LottieLearning})
+    void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.LinearGradientLearning:
+                intent = new Intent(this, LinearGradientActivity.class);
+                break;
+            case R.id.LottieLearning:
+                intent = new Intent(this, LottieActivity.class);
+                break;
+        }
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/com/chk/animationlearning/ui/lottie/LottieActivity.java
@@ -0,0 +1,16 @@
+package com.chk.animationlearning.ui.lottie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chk.animationlearning.R;
+
+public class LottieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lottie);
+    }
+}
